44484129a3dcfcac7aac131fad7295fd9b85c8d2
mssinfotech <mragankshekhar@ms2fun.com>
2017-08-29 06:42:08 +0530

    reg code added Signed-off-by:mssinfotech <mragankshekhar@ms2fun.com>

--- api/common.php
+++ api/common.php
@@ -1,17 +1,23 @@
 <?php
+
 include("../config.php");
 $ch = strtolower($_REQUEST["type"]);
-if($ch== "update-language") {
-	$lang=$_REQUEST["lang_my"];
-	$_SESSION['lang'] = $lang;
-	setcookie('lang', $lang, time() + (3600 * 24 * 30));
-	echo "langSet---".$lang;
-}elseif($ch== "update-latlong") {
-	$lat=$_REQUEST["lat_my"];
-	$long=$_REQUEST["long_my"];
-	$_SESSION['lat'] = $lang;
-	$_SESSION['long'] = $lang;
-	setcookie('long', $long, time() + (3600 * 24 * 30));
-	setcookie('lat', $lat, time() + (3600 * 24 * 30));
-	echo "langSet---".$long."---".$lat;
+if ($ch == "update-language") {
+    $lang = $_REQUEST["lang_my"];
+    $_SESSION['lang'] = $lang;
+    setcookie('lang', $lang, time() + (3600 * 24 * 30));
+    echo "langSet---" . $lang;
+} elseif ($ch == "update-reg_id") {
+    $reg_id = $_REQUEST["reg_id"];
+    $_SESSION['reg_id'] = $reg_id;
+    setcookie('reg_id', $reg_id, time() + (3600 * 24 * 30));
+    echo "reg_idSet---" . $reg_id;
+} elseif ($ch == "update-latlong") {
+    $lat = $_REQUEST["lat_my"];
+    $long = $_REQUEST["long_my"];
+    $_SESSION['lat'] = $lang;
+    $_SESSION['long'] = $lang;
+    setcookie('long', $long, time() + (3600 * 24 * 30));
+    setcookie('lat', $lat, time() + (3600 * 24 * 30));
+    echo "langSet---" . $long . "---" . $lat;
 }
